00195277b124bef583c17da2d129dd304d62a215
Kennedy Alves <kennedyalves.ka@gmail.com>
2018-08-13 20:10:15 -0300

    Adicionado código do banco de dados

--- src/java/LoginServlets.java
+++ src/java/LoginServlets.java
@@ -1,121 +1,97 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Kennedy
- */
 @WebServlet(urlPatterns = {"/LoginServlets"})
 public class LoginServlets extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet LoginServlets</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.print("<form action='LoginServlets' method'post' >");
-            out.print("<label>Usuário:<input type='text' name='nome' /></label><br/>");
-            out.print("<label>Senha:<input type='password' name='senha' /></label><br/>");
-            out.print("<button type'submit'>Logar</button>");
+            out.print("<form action='LoginServlets' method='post' >");
+            out.println("<label>Usuario:<input type='text' name='nome' /></label><br/>");
+            out.println("<label>Senha:<input type='password' name='senha' /></label><br/>");
+            out.println("<button type='submit'>Logar</button>");
             out.print("</form>");
             
-//            if ( "guiGay".equals(request.getParameter("nome"))){
-//                out.println("<br/>Isso é verdade ...");
-//                }
-//            else if ( !"".equals(request.getParameter("nome")) && request.getParameter("nome") != null) {
-//                out.println("<br/>Nome de Login Válido ...");
-//            }
+    Class.forName("com.mysql.jdbc.Driver");
+    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/servlet", "root", "");
 
-        if ( loginChech ( request.getParameter("nome")) && loginChech ( request.getParameter("senha"))){
-            if ( request.getParameter("nome").equals("Fulano") && request.getParameter("senha").equals("iesb")){
-                response.sendRedirect("MenuServlet");
-                /* RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/menuServlet");
-                dispatcher.forward(request, response);
-                */
-            }
-            else if ( !"".equals(request.getParameter("nome")) && request.getParameter("nome") != null) {
-                out.println("<br/>Insira um login ou senha válido");
-            }
-            else {
-                out.print("Senha ou login não aprovado(s)");
-            }
+    Statement st = con.createStatement();
+
+
+    ResultSet rs = st.executeQuery("select * from logins");
+      
+     
+      while (rs.next())
+      {
+        int id = rs.getInt("id");
+        String usuario = rs.getString("usuario");
+        String senha = rs.getString("senha");
+        
+        if( usuario.equals(request.getParameter("nome")) && senha.equals(request.getParameter("senha"))){
+            HttpSession session = request.getSession(true);
+            session.setAttribute("codigo", "123");
+
+            RequestDispatcher dispatcher = getServletContext()
+            .getRequestDispatcher("/MenuServlet"); 
+          dispatcher.forward(request, response);
         }
-            
-            out.println("</body>");
-            out.println("</html>");
+        
+      }
+      st.close();
+
+        out.println("</body>");
+        out.println("</html>");
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginServlets.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlets.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
-    
-    protected boolean loginChech(String valor){
-        if ( !"".equals(valor) && valor != null)
-            return true;
-        else
-            return false;
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginServlets.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlets.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
+  
 }
