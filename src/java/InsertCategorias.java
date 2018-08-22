9cfe6982ad497b738323c9a8d38bb0becb8fdf6c
Kennedy Alves <kennedyalves.ka@gmail.com>
2018-08-22 20:32:14 -0300

    [UPDATE] Atualizado Inserts de categorias

--- src/java/InsertCategorias.java
+++ src/java/InsertCategorias.java
@@ -2,69 +2,90 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-@WebServlet(urlPatterns = {"/CategoriaServlet"})
+@WebServlet(urlPatterns = {"/InsertCategorias"})
 public class InsertCategorias extends HttpServlet {
-    
-   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, ClassNotFoundException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
+            
+            String query = "select * from categorias", html="";    
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/servlet", "root", "");
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>::.. LISTAGEM DE PRODUTO ..::</title>");            
+            out.println("<title>Servlet InsertCategorias</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>LISTAGEM CATEGORIA</h1>");
-            
-    Class.forName("com.mysql.jdbc.Driver");
-    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/servlet", "root", "");
-
-    Statement st = con.createStatement();
-
-    String query = "select * from categorias";
+            out.println("<form action='InsertCategorias' method='POST'>");
+            out.println("<label>Nome:<input type='text' name='nome'/></label>");
+            out.println("<select name='categorias_id'>");
  
-    ResultSet rs = st.executeQuery(query);
-      
-      out.println("<table>");
-      out.println("<tr>");
-      out.println("<th>id</th>");
-      out.println("<th>NOME</th>");
-      out.println("<th>CATEGORIA ID</th>");
-      
-      while (rs.next())
-      {
-         
-          out.println("<tr>");
-          out.println("<td>");
-          out.println(rs.getInt("id"));
-          out.println("</td>");
-          out.println("<td>");
-          out.println(rs.getString("nome"));
-          out.println("</td>");
-          out.println("<td>");
-          out.println(rs.getString("categorias_id"));
-          out.println("</td>");
-          out.println("</tr>");
-        
-      }
-      st.close();
-        out.println("</table>");
-        out.println("</body>");
-        out.println("</html>");
+            PreparedStatement stmt = con.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery(query);
+            
+            while(rs.next()){
+                out.println("<option value='"+rs.getInt("id")+"'>"+rs.getString("nome")+"</option>");
+                html +="<tr>";
+                html +="<td>";
+                html +=rs.getInt("id");
+                html +="</td>";
+                html +="<td>";
+                html +=rs.getString("nome");
+                html +="</td>";
+                html +="<td>";
+                html +=rs.getString("categorias_id");
+                html +="</td>";
+                html +="</tr>";
+            }
+            out.println("</select>");
+            out.println("<button type='submit'>Enviar</button>");
+            out.println("</form>");
+            out.println("<h1>LISTA DE CATEGORIAS</h1>");
+            out.println("<table>");
+            out.println("<tr>");
+            out.println("<th>");
+            out.println("ID");
+            out.println("</th>");
+            out.println("<th>");
+            out.println("NOME");
+            out.println("</th>");
+            out.println("<th>");
+            out.println("CATEGORIAS");
+            out.println("</th>");
+            out.println("</tr>");
+            out.println(html);
+            
+            out.println("</table>");
+            
+            out.println("</body></html>");
+            query = "insert into categorias(nome, categorias_id) values(?,?)";
+            
+            if( request.getParameter("nome")!= null){
+               
+                PreparedStatement preparedStmt = con.prepareStatement(query);
+                preparedStmt.setString (1, request.getParameter("nome"));
+                preparedStmt.setString (2, request.getParameter("categorias_id"));
+                preparedStmt.execute();
+                con.close();
+            }
         }
     }
 
@@ -75,9 +96,9 @@ public class InsertCategorias extends HttpServlet {
         try {
             processRequest(request, response);
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(LoginServlets.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(InsertCategorias.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
-            Logger.getLogger(LoginServlets.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(InsertCategorias.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
@@ -88,12 +109,13 @@ public class InsertCategorias extends HttpServlet {
         try {
             processRequest(request, response);
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(LoginServlets.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(InsertCategorias.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
-            Logger.getLogger(LoginServlets.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(InsertCategorias.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-
-  
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
 }
